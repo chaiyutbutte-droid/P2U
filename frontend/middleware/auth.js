c14cb9465ff60f29312e018cf95c41f93f324643
artuchicabo <chaiyutbutte@gmail.com>
2026-01-10 18:48:16 +0700

    11

--- frontend/middleware/auth.js
+++
@@ -1,6 +0,0 @@
-export default defineNuxtRouteMiddleware(() => {
-  const token = localStorage.getItem('token')
-  if (!token) {
-    return navigateTo('/login')
-  }
-})
--- frontend/pages/index.vue
+++
@@ -1,5 +0,0 @@
-<!-- pages/index.vue -->
-<script setup>
-navigateTo('/login')  // redirect ไปหน้า login
-</script>
-
